34ed4c1c2bfb4ade67faf23162f8f653b437deec
Dana Shorts <32418263+csudhtoro@users.noreply.github.com>
2020-01-13 22:44:26 -0800

    Initial commit

---
+++ Assets/Scripts/ballHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballHandler : MonoBehaviour
+{
+
+    public float ballSpeed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * ballSpeed;
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 paddlePos, float paddleWidth)
+    {
+        return (ballPos.x - paddlePos.x) / paddleWidth;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "paddle")
+        {
+            float x = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * ballSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/racketHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racketHandler : MonoBehaviour
+{
+    public float xBound = 3.0f;
+    public string axis = "Horizontal";
+    public float paddleSpeed = 150;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * paddleSpeed;
+
+        float p = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(p, 0) * paddleSpeed;
+        var pos = transform.position;
+        if (pos.x > xBound) pos.x = xBound;
+        else if (pos.x < -xBound) pos.x = -xBound;
+    }
+
+    /*void paddleBound()
+    {
+        float h = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(h, 0) * paddleSpeed;
+
+        var pos = transform.position;
+        if (pos.x > xBound) pos.x = xBound;
+        else if (pos.x < -xBound) pos.x = -xBound;
+    }*/
+}
