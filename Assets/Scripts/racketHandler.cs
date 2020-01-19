89e9d69424b4c351a96a27d2b319a52f73cb73a3
Dana Shorts <32418263+csudhtoro@users.noreply.github.com>
2020-01-18 17:42:01 -0800

    Fixed border physics, added blocks and rng for powerups

--- Assets/Scripts/racketHandler.cs
+++ Assets/Scripts/racketHandler.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class racketHandler : MonoBehaviour
 {
+    
     public float xBound = 3.0f;
     public string axis = "Horizontal";
     public float paddleSpeed = 150;
+    
 
     // Update is called once per frame
     void FixedUpdate()
@@ -16,18 +18,12 @@ public class racketHandler : MonoBehaviour
 
         float p = Input.GetAxisRaw(axis);
         GetComponent<Rigidbody2D>().velocity = new Vector2(p, 0) * paddleSpeed;
-        var pos = transform.position;
-        if (pos.x > xBound) pos.x = xBound;
-        else if (pos.x < -xBound) pos.x = -xBound;
+        
     }
 
-    /*void paddleBound()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        float h = Input.GetAxisRaw(axis);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(h, 0) * paddleSpeed;
 
-        var pos = transform.position;
-        if (pos.x > xBound) pos.x = xBound;
-        else if (pos.x < -xBound) pos.x = -xBound;
-    }*/
+    }
+
 }
